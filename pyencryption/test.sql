b59c079ab0f21d44d2a68b1b8b9acd63eb126843
Attapon Thanawong <attapon_srem@hotmail.com>
2024-04-17 14:28:11 +0700

    Modified: pyencryption/test.sql

--- pyencryption/test.sql
+++ pyencryption/test.sql
@@ -1,9 +1,5 @@
 
-DROP TABLE IF EXISTS test_pyencrypt;
-CREATE TEMP TABLE test_pyencrypt ON COMMIT PRESERVE ROWS AS
-SELECT pyencrypt(p.CID , 'test-password') ecid FROM hdc_cmi.PERSON p  WHERE thaimod11(p.CID) = 1 LIMIT 10;
+select pyencrypt('test', 'test-pass');
 
-SELECT * FROM test_pyencrypt;
 
-SELECT  pydecrypt(ecid, 'test-password') FROM test_pyencrypt;
-DROP TABLE IF EXISTS test_pyencrypt;
+select pydecrypt(pyencrypt('test', 'test-pass'), 'test-pass');
