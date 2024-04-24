b04f12d395984907389b7e8c818b00490aee68fe
Attapon Thanawong <attapon_srem@hotmail.com>
2024-04-24 13:46:59 +0700

    Modified: Python/ddl/test.sql

--- Python/ddl/test.sql
+++ Python/ddl/test.sql
@@ -13,4 +13,4 @@ SELECT 'Test Base64 Encoding' title;
 select 'test-test-encode' ptxt,  vb64encode('test-test-encode') entext,  vb64decode(vb64encode('test-test-encode') ) detext;
 
 SELECT 'Test thaimod11' title;
-SELECT '1234567890123' cid_f, thaimod11('1234567890123') f, '1893535345732' cid_t thaimod11('1893535345732') t;
+SELECT '1234567890123' cid_f, thaimod11('1234567890123') f, '1893535345732' cid_t, thaimod11('1893535345732') t;
